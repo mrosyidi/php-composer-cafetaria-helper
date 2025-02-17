4351d8a3322dd4dfeceb51ce63d69ba66dd71cde
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-17 23:22:26 +0700

    add DataHelperTest.php

---
+++ tests/DataHelperTest.php
@@ -0,0 +1,44 @@
+<?php 
+
+    require_once __DIR__ . "/../vendor/autoload.php";
+
+    class Food
+    {
+        private string $name;
+        private int $price;
+
+        public function __construct(string $name, int $price)
+        { 
+            $this->name = $name;
+            $this->price = $price; 
+        }
+
+        public function getName(): string{ return $this->name; }
+        public function getPrice(): int{ return $this->price; }
+    }
+
+    class Drink 
+    {
+        private string $name;
+        private int $price;
+
+        public function __construct(string $name, int $price)
+        { 
+            $this->name = $name; 
+            $this->price = $price;
+        }
+
+        public function getName(): string{ return $this->name; }
+        public function getPrice(): int{  return $this->price; }
+    }
+
+    function testDataHelperFood(): void 
+    {
+        $foods[] = new Food("Mie Ayam", 6000);
+        $foods[] = new Food("Rawon", 12000);
+        $foods[] = new Food("Soto Ayam", 10000);
+        $data = \Cafetaria\Helper\DataHelper::data($foods, 1);
+        var_dump($data);
+    }
+
+    testDataHelperFood();
