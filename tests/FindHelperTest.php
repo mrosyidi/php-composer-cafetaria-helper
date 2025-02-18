17a461198d6fc21bdc2f01d8cc5579806ffd5a02
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-18 11:49:01 +0700

    add FindHelperTest.php

---
+++ tests/FindHelperTest.php
@@ -0,0 +1,19 @@
+<?php 
+
+    require_once __DIR__ . "/../vendor/autoload.php";
+
+    class Order 
+    {
+        private int $code;
+        public function __construct(int $code){ $this->code = $code; }
+        public function getCode(): int { return $this->code; }
+    }
+
+    function testFindHelperNotFound(): void 
+    {
+        $orders = [];
+        $result = \Cafetaria\Helper\FindHelper::find($orders, 2);
+        var_dump($result);
+    }
+
+    testFindHelperNotFound();
